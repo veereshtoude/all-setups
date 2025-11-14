ebca1049cfbe578b3c228d9cf0d05e99fe7ac110
veereshtoude <tveeresh04@gmail.com>
2025-11-14 18:38:18 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,4 +1,4 @@
-amazon-linux-extras install java-openjdk11 -y
+yum install java-17-amazon-corretto -y
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.112/bin/apache-tomcat-9.0.112.tar.gz
 tar -zxvf apache-tomcat-9.0.112.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.112/conf/tomcat-users.xml
